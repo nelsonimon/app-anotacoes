4bcb84d2ade597f4e764897df4ce5d8891b81675
nelsonimon <nelson_imon@yahoo.com.br>
2021-09-14 11:24:53 -0300

    adicionado mix.webpackConfig para corrigir referencia @

--- public/js/resources_js_components_notes_NoteIndex_vue.js
+++ public/js/resources_js_components_notes_NoteIndex_vue.js
@@ -11,7 +11,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-/* harmony import */ var _services_NoteDataService__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../services/NoteDataService */ "./resources/js/services/NoteDataService.js");
+/* harmony import */ var _services_NoteDataService__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @/services/NoteDataService */ "./resources/js/services/NoteDataService.js");
 //
 //
 //
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,16 @@ const mix = require('laravel-mix');
  |
  */
 
+ mix.webpackConfig({
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      '@': __dirname + '/resources/js'
+    },
+  },
+})
+
 mix.js('resources/js/app.js', 'public/js')
     .vue()
     .sass('resources/sass/app.scss', 'public/css');
